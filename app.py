2d3a074ce7d6fc0641f4b24e572f784b1ac92f07
xoxo-cyber <61509033+xoxo-cyber@users.noreply.github.com>
2026-01-03 00:02:54 +0100

    app.py

--- app.py
+++ app.py
@@ -42,3 +42,8 @@ def process():
         "captures": images,
         "count": len(images)
     })
+
+# 🔥 OBLIGATOIRE POUR RENDER
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
